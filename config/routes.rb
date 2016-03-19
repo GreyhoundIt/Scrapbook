dd0d7937d2a5f6bb8e221d8c1311d62909e8c9cf
Matthew <matthew@greyhoundit.co.uk>
2016-03-19 08:06:20 +0000

    all code done

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   get 'static_pages/index'
 
-  root 'static_pages#index'
-
   resources :docs
+  authenticated :user do
+    root "docs#index", as: "authenticated_root"
+  end
+  root 'static_pages#index'
 end
